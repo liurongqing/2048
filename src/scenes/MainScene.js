39459a5edfd3478079e8dec6a711823055f6af94
liurongqing <phpgege@163.com>
2018-06-22 18:07:58 +0800

    1

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,8 +1,10 @@
 import { COL, ROW, TILE_SIZE, TILE_SPACING, TWEEN_DURATION, LOCAL_STORAGE_NAME } from '../constant'
+import { runInNewContext } from 'vm';
 
 class MainScene extends Phaser.Scene {
 
     tileArray = [];
+    canMove = false;
 
     constructor() {
         super({
@@ -101,46 +103,249 @@ class MainScene extends Phaser.Scene {
     }
 
     handleKey = (e) => {
-        let children = this.tileGroup.getChildren();
-        switch (e.code) {
-            case "KeyA":
-            case "ArrowLeft":
-                for (var i = 0; i < children.length; i++) {
+        if (this.canMove) {
+            let children = this.tileGroup.getChildren();
+            switch (e.code) {
+                case "KeyA":
+                case "ArrowLeft":
+                    for (var i = 0; i < children.length; i++) {
 
-                    // 越向右层级越高
-                    children[i].depth = children[i].x;
-                }
-                console.log('向左')
-                break;
-            case "KeyD":
-            case "ArrowRight":
-                for (var i = 0; i < children.length; i++) {
+                        // 越向右层级越高
+                        children[i].depth = children[i].x;
+                    }
+                    console.log('向左')
 
-                    // 设置层级，越左侧层级越高
-                    children[i].depth = game.config.width - children[i].x;
-                }
-                console.log('向右')
-                break;
-            case "KeyW":
-            case "ArrowUp":
-                for (var i = 0; i < children.length; i++) {
+                    // 行为 0，不变化，列为 -1，向左
+                    this.move(0, -1);
+                    break;
+                case "KeyD":
+                case "ArrowRight":
+                    for (var i = 0; i < children.length; i++) {
 
-                    // 越下面层级越高
-                    children[i].depth = children[i].y;
-                }
-                console.log('向上')
-                break;
-            case "KeyS":
-            case "ArrowDown":
-                for (var i = 0; i < children.length; i++) {
+                        // 设置层级，越左侧层级越高
+                        children[i].depth = game.config.width - children[i].x;
+                    }
+                    console.log('向右')
 
-                    // 越上面层级越高
-                    children[i].depth = game.config.height - children[i].y;
-                }
-                console.log('向下')
-                break;
+                    // 行为 0，不变化，列为 1，向右
+                    this.move(0, 1);
+                    break;
+                case "KeyW":
+                case "ArrowUp":
+                    for (var i = 0; i < children.length; i++) {
+
+                        // 越下面层级越高
+                        children[i].depth = children[i].y;
+                    }
+                    console.log('向上')
+
+                    // 列为 0，不变化，行为 -1，向上
+                    this.move(-1, 0);
+                    break;
+                case "KeyS":
+                case "ArrowDown":
+                    for (var i = 0; i < children.length; i++) {
+
+                        // 越上面层级越高
+                        children[i].depth = game.config.height - children[i].y;
+                    }
+                    console.log('向下')
+
+                    // 列为 0，不变化，行为 1，向下
+                    this.move(1, 0);
+                    break;
+            }
         }
     }
+
+
+    /**
+     * 
+     * @param stepRow 行的变化，上下变化，上下滑动，0 为不变化， 1为进1块  -1为退1块
+     * @param stepCol 列的变化，左右变化，左右滑动，0 为不变化， 1为进1块  -1为退1块
+     */
+    move(rowStep, colStep) {
+        this.canMove = false;
+        this.movingTiles = 0;
+
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+
+                /**
+                 * 一块一块移动去判断
+                 * 正确的移动顺序应该是：
+                 * 向左移动（move(0, -1)）：从左到右，从上到下遍历判断移动【默认遍历 i，j 即可】
+                 * 向右移动（move(0, 1)） ：从右到左，从上到下遍历判断移动【默认遍历 i，j 取反值】
+                 * 向上移动（move(-1, 0） ：从左到右，从上到下遍历判断移动【默认遍历 i，j 即可】
+                 * 向下移动（move(1, 0)） ：从左到右，从下到上遍历判断移动【默认遍历 j，i 取反值】
+                 */
+                let row = rowStep === 1 ? (3 - i) : i;
+                let col = colStep === 1 ? (3 - j) : j;
+
+                let tileValue = this.tileArray[row][col].tileValue;
+                if (tileValue !== 0) {
+
+                    // 横向移动单位距离
+                    let rowSteps = rowStep;
+
+                    // 纵向移动单位距离
+                    let colSteps = colStep;
+
+                    // 条件一：靠边移动，不可超出
+                    // 条件二：移动的地方没有数值方块
+                    while (this.isInsideBoard(row + rowSteps, col + colSteps) && this.tileArray[row + rowSteps][col + colSteps].tileValue === 0) {
+                        colSteps += colStep;
+                        rowSteps += rowStep;
+                    }
+
+                    // 条件一：靠边移动，不可超出
+                    // 条件二：目标方块与当前方块 tileValue 相等，也就是数值相等
+                    // 条件三：目标方块 canUpgrade 为 true【控制一次只能被覆盖一次】
+                    // 条件四：当前方块 canUpgrade 为 true
+                    // 条件五：tileValue 小于 12 Math.pow(2,12) 4096，最大 4096
+                    if (this.isInsideBoard(row + rowSteps, col + colSteps) &&
+                        (this.tileArray[row + rowSteps][col + colSteps].tileValue === tileValue) &&
+                        this.tileArray[row + rowSteps][col + colSteps].canUpgrade &&
+                        this.tileArray[row][col].canUpgrade &&
+                        tileValue < 12) {
+
+                        // 目标方块 tileValue + 1， 本来是 Math.pow(2,1) 变成了 Math.pow(2,2)，也就是方块 2 变成 4
+                        this.tileArray[row + rowSteps][col + colSteps].tileValue++;
+
+                        // 目标块只能被覆盖一次
+                        this.tileArray[row + rowSteps][col + colSteps].canUpgrade = false;
+
+                        // 设置当前方块 tileValue 为 0
+                        this.tileArray[row][col].tileValue = 0;
+
+                        // 当前方块移动到目标方块
+                        // 参数一：当前数值精灵
+                        // 参数二：横向位置
+                        // 参数三：纵向位置
+                        // 参数四：移动单位距离
+                        // 参数五： bool
+                        this.moveTile(this.tileArray[row][col], row + rowSteps, col + colSteps, Math.abs(rowSteps + colSteps), true);
+                    } else {
+
+                        // while 时最后一次条件不成立，但 colSteps 与 rowSteps 已经加了col与row，所以这里减回去。
+                        rowSteps = rowSteps - rowStep;
+                        colSteps = colSteps - colStep;
+
+                        // 若横向或纵向有移动，则开始移动
+                        if (colSteps !== 0 || rowSteps !== 0) {
+
+                            // console.log(row, rowSteps, col, colSteps)
+                            // 设置移动到的地方值为当前值
+                            this.tileArray[row + rowSteps][col + colSteps].tileValue = tileValue;
+
+                            // 设置当前块的值为 0
+                            this.tileArray[row][col].tileValue = 0;
+
+                            // 参数一：精灵
+                            // 参数二：横向位置
+                            // 参数三：纵向位置
+                            // 参数四：移动单位距离
+                            // 参数五： bool
+                            this.moveTile(this.tileArray[row][col], row + rowSteps, col + colSteps, Math.abs(rowSteps + colSteps), false);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param {Sprite} tile 
+     * @param {Number} row 
+     * @param {Number} col 
+     * @param {Number} distance 根据距离动态改变动画运行时间
+     * @param {Booler} changeNumber 
+     */
+    moveTile(tile, row, col, distance, changeNumber) {
+
+        this.movingTiles++;
+        this.tweens.add({
+            targets: [tile.tileSprite],
+            x: this.setPosition(col, COL),
+            y: this.setPosition(row, ROW),
+            duration: TWEEN_DURATION * distance,
+            onCompolete: () => {
+                this.movingTiles--;
+
+                if (changeNumber) {
+                    this.transformTile(tile, row, col);
+                }
+                if (this.movingTiles === 0) {
+                    console.log('reset...')
+                    this.resetTiles();
+                    this.addTile();
+                }
+
+            }
+            // onComplete: function (tween) {
+            //     tween.parent.scene.movingTiles--;
+
+            //     // 数字有变化，则更新加动画
+            //     if (changeNumber) {
+            //         tween.parent.scene.transformTile(tile, row, col);
+            //     }
+
+            //     // 全部移动完以后
+            //     if (tween.parent.scene.movingTiles == 0) {
+            //         tween.parent.scene.scoreText.text = tween.parent.scene.score.toString();
+            //         tween.parent.scene.bestScoreText.text = tween.parent.scene.bestScore.toString();
+            //         tween.parent.scene.resetTiles();
+            //         tween.parent.scene.addTile();
+            //     }
+            // }
+        })
+    }
+
+    transformTile(tile, row, col) {
+        this.movingTiles++;
+        tile.tileSprite.setFrame(this.tileArray[row][col].tileValue - 1);
+        this.tweens.add({
+            targets: [tile.tileSprite],
+            scaleX: 1.1,
+            scaleY: 1.1,
+            duration: TWEEN_DURATION,
+            yoyo: true,
+            repeat: 1,
+            onComplete: function () {
+                this.movingTiles--;
+                if (this.movingTiles === 0) {
+                    console.log('reset...')
+                    this.resetTiles();
+                    this.addTile();
+                }
+                console.log('transform')
+            }
+        })
+    }
+
+    // 精灵归位，数组中指定的 tileValue 信息来显示数组中
+    resetTiles() {
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                this.tileArray[i][j].canUpgrade = true;
+                this.tileArray[i][j].tileSprite.x = this.setPosition(j, COL);
+                this.tileArray[i][j].tileSprite.y = this.setPosition(i, ROW);
+                if (this.tileArray[i][j].tileValue > 0) {
+                    this.tileArray[i][j].tileSprite.alpha = 1;
+                    this.tileArray[i][j].tileSprite.visible = true;
+
+                    // 假如 tileValue = 2，则方块数值为 Math.pow(2,2) == 4, 4 的精灵索引为1，所以等于 tileValue - 1
+                    this.tileArray[i][j].tileSprite.setFrame(this.tileArray[i][j].tileValue - 1);
+                } else {
+                    this.tileArray[i][j].tileSprite.alpha = 0;
+                    this.tileArray[i][j].tileSprite.visible = false;
+                }
+            }
+        }
+    }
+
+
 
     // 布局
     layout() {
@@ -247,9 +452,14 @@ class MainScene extends Phaser.Scene {
                 alpha: 1,
                 duration: TWEEN_DURATION,
                 onCompolete: () => {
+                    this.canMove = true;
                 }
             });
         }
+    }
+
+    isInsideBoard(row, col) {
+        return (row >= 0) && (col >= 0) && (row < 4) && (col < 4);
     }
 
 
